86191cfc3f5aea39b441aaf2d6aa6af505d22ea1
AkilovM <39463269+AkilovM@users.noreply.github.com>
2023-05-06 21:55:19 +0500

    Models were created, migrated to SQLite.

--- corp_food_order_web_app/main_app/models.py
+++ corp_food_order_web_app/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+
+class Food(models.Model):
+    name = models.CharField(max_length=30)
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    
+
+class Order(models.Model):
+    date = models.DateField()
+    person = models.ForeignKey(Person, on_delete = models.CASCADE)
+
+class Food_Order(models.Model):
+    food = models.ForeignKey(Food, on_delete = models.CASCADE)
+    order = models.ForeignKey(Order, on_delete = models.CASCADE)
+    amount = models.PositiveSmallIntegerField()
