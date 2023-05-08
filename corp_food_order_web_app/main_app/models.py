fdf7fb0d926db3310a2fca2aee068dbdd0b93c66
AkilovM <39463269+AkilovM@users.noreply.github.com>
2023-05-09 04:25:23 +0500

    Added human-readable representation of model instances.

--- corp_food_order_web_app/main_app/models.py
+++ corp_food_order_web_app/main_app/models.py
@@ -4,17 +4,28 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Food(models.Model):
     name = models.CharField(max_length=30)
     ingredients = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Order(models.Model):
     date = models.DateField()
     person = models.ForeignKey(Person, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return f'{self.date} {self.person}'
+
 class Food_Order(models.Model):
     food = models.ForeignKey(Food, on_delete = models.CASCADE)
     order = models.ForeignKey(Order, on_delete = models.CASCADE)
     amount = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.food} {self.order} {self.amount}'
