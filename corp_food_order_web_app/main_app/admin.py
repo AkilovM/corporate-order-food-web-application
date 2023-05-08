3f9f66776532a854d7fd81af18b8a0bb3ae88add
AkilovM <39463269+AkilovM@users.noreply.github.com>
2023-05-09 01:54:02 +0500

    Added models to admin.py, few visual changes on index.html.

--- corp_food_order_web_app/main_app/admin.py
+++ corp_food_order_web_app/main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Person, Food, Order, Food_Order
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Food)
+admin.site.register(Order)
+admin.site.register(Food_Order)
