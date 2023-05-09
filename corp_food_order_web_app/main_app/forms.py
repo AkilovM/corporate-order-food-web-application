0b6d2eb87a22f505a604ac6a3bb3f103f02a3caf
AkilovM <39463269+AkilovM@users.noreply.github.com>
2023-05-09 18:20:43 +0500

    Code refactoring.

--- corp_food_order_web_app/main_app/forms.py
+++ corp_food_order_web_app/main_app/forms.py
@@ -1,11 +1,6 @@
 from django import forms
 from .models import Person, Food, Order, Food_Order
 
-# Represent person as person.name
-class Person_ModelChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return obj.name
-
 class Food_Amount():
     def __init__(self, food, amount):
         self.food = food
@@ -19,27 +14,17 @@ class Food_Count_Fields(forms.MultiValueField):
         fields = tuple(fields)
 
 class Order_Form(forms.Form):
-    #person = Person_ModelChoiceField(queryset=Person.objects.all(), label='Ваше имя:')
     date = forms.DateField(widget=forms.SelectDateWidget(), label='Дата заказа:')
-
     fields = []
-
     food_list = []
     menu = Food.objects.all()
     for food in menu:
         food_amount_dict = dict()
         food_amount_dict['food'] = food
         food_amount_dict['amount'] = forms.IntegerField(min_value=0, max_value=99)
-        #food_list.append(Food_Amount(food, forms.IntegerField(min_value=0, max_value=99)))
         food_list.append(food_amount_dict)
         fields.append(forms.IntegerField(min_value=0, max_value=99))
-
-
     amount_fields = forms.MultiValueField(fields=fields)
 
 class Date_Form(forms.Form):
     date = forms.DateField(widget=forms.SelectDateWidget(), label='Дата заказа:')
-
-
-
-            
--- corp_food_order_web_app/main_app/views.py
+++ corp_food_order_web_app/main_app/views.py
@@ -4,28 +4,28 @@ from .models import Person, Food, Order, Food_Order
 from .forms import Order_Form, Date_Form
 import copy
 
+def get_date_from_post(request):
+    day = request.POST.get('date_day')
+    month = request.POST.get('date_month')
+    year = request.POST.get('date_year')
+    date = year+'-'+month+'-'+day # YYYY-MM-DD
+    return date
+
 # Create your views here.
 def index(request):
-    #create_test_values()
-    #print(Order.objects.all()[1].person)
     persons = Person.objects.all()
     menu = Food.objects.all()
     order_form = Order_Form()
     if request.method == 'GET':
         return render(request, 'index.html', {'persons':persons, 'menu':menu, 'order_form':order_form})
     elif request.method == 'POST':
-        #print(request.POST)
         order = Order()
-        day = request.POST.get('date_day')
-        month = request.POST.get('date_month')
-        year = request.POST.get('date_year')
-        date = year+'-'+month+'-'+day # YYYY-MM-DD
+        date = get_date_from_post(request)
         order.date = date
         order.person = Person.objects.get(pk=request.POST.get('person'))
         food_amounts = request.POST.getlist('amount')
         # TODO check that at least 1 amount > 0
         order.save()
-
         if len(menu) == len(food_amounts):
             for i in range(len(food_amounts)):
                 food_order = Food_Order()
@@ -44,9 +44,9 @@ def history(request):
     elif request.method == 'POST':
         person = request.POST.get('person')
         orders = Order.objects.filter(person=person)
-        for o in orders:
-            foods = Food_Order.objects.filter(order=o)
-            o.foods = foods
+        for order in orders:
+            foods = Food_Order.objects.filter(order=order)
+            order.foods = foods
         return render(request, 'history.html', {'persons':persons, 'person':Person.objects.get(pk=person), 'orders':orders})
             
 def report(request):
@@ -54,22 +54,16 @@ def report(request):
     if request.method == 'GET':
         return render(request, 'report.html', {'date_form':date_form})
     elif request.method == 'POST':
-        day = request.POST.get('date_day')
-        month = request.POST.get('date_month')
-        year = request.POST.get('date_year')
-        date = year+'-'+month+'-'+day # YYYY-MM-DD
+        date = get_date_from_post(request)
         orders = Order.objects.filter(date=date)
-
         overall_sum = 0
         food_dict = dict()
-        
         for order in orders:
             food_orders = Food_Order.objects.filter(order=order)
             for food_order in food_orders:
                 food = food_order.food
                 amount = food_order.amount
                 if food.name not in food_dict.keys():
-                    #food_data = object()
                     food_data = copy.copy(food)
                     food_data.amount = 0
                     food_data.name = food.name
@@ -79,24 +73,5 @@ def report(request):
                 food_dict[food.name].amount += amount
                 food_dict[food.name].sum += amount * food.price
                 overall_sum += amount * food.price
-
         food_list = food_dict.values()
         return render(request, 'report.html', {'date_form':date_form, 'date':date, 'food_list':food_list, 'overall_sum':overall_sum})
-
-def create_test_values():
-    if Person.objects.all().count() == 0:
-        for name in ['Александр', 'Евгения', 'Юрий', 'Юлия', 'Иван', 'Мария']:
-            Person.objects.create(name=name)
-    if Food.objects.all().count() == 0:
-        Food.objects.create(name='Суп', ingredients='Вода 200мл, Капуста, Морковь, Вода 200мл, Капуста, Морковь, Вода 200мл, Капуста, Морковь.', price=50.99)
-        Food.objects.create(name='Салат', ingredients='Огурец, Помидор, Капуста, Лук', price=40.49)
-        Food.objects.create(name='Чай', ingredients='Заварка, Сахар.', price=7.50)
-    if Order.objects.all().count() == 0:
-        person = Person.objects.all()[0]
-        Order.objects.create(date='2023-01-01', person=person)
-    if Food_Order.objects.all().count() == 0:
-        menu = Food.objects.all()
-        order = Order.objects.all()[0]
-        food_order = Food_Order.objects.create(food=menu[0], order=order, amount=1)
-        food_order = Food_Order.objects.create(food=menu[1], order=order, amount=2)
-        food_order = Food_Order.objects.create(food=menu[2], order=order, amount=3)
